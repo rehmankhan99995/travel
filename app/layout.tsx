c49eb33b0f221aff0702183d3f3d6100449d0d23
Abdullah <ab1212123132@gmail.com>
2025-10-30 19:01:59 +0500

    edit sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Sidebar } from "@/components/ui/sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>
+          <div>
+            <Sidebar />
+          </div>
+          <div>
         {children}
+        </div>
+        </div>
       </body>
     </html>
   );
